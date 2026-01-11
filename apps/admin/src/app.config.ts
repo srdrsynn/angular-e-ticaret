389fa7699b4212fbce0ccc78a84cddf44a7c984b
Serdar SAYIN <srdrsynbtbss@gmail.com>
2026-01-11 16:35:29 +0300

    footer düzenlemesi yapıldı.

--- apps/admin/src/app.config.ts
+++ apps/admin/src/app.config.ts
@@ -1,14 +1,23 @@
 import {
   ApplicationConfig,
+  LOCALE_ID,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
+
+import { registerLocaleData } from '@angular/common';
+import localeTr from '@angular/common/locales/tr';
+
+registerLocaleData(localeTr);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(appRoutes)],
+    provideRouter(appRoutes),
+    { provide: LOCALE_ID, useValue : 'tr' }
+  ],
 };
--- apps/admin/src/pages/layouts/layouts.ts
+++ apps/admin/src/pages/layouts/layouts.ts
@@ -4,6 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { navigations } from '../../navigation';
 import { NavPipe } from '../../pipes/nav-pipe';
 import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   imports: [
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
     RouterLink, 
     RouterLinkActive, 
     NavPipe,
-    FormsModule
+    FormsModule,
+    DatePipe
   ],
   templateUrl: './layouts.html',
   encapsulation: ViewEncapsulation.None,
@@ -19,5 +21,13 @@ import { FormsModule } from '@angular/forms';
 })
 export default class Layouts {
   readonly search = signal<string>('');
+  readonly time = signal<Date | string>('');
   readonly navigations = computed(() => navigations);
+
+  constructor() {
+    setInterval(() => {
+      this.time.set(new Date());
+    }, 1000);   
+  }
+
 }
